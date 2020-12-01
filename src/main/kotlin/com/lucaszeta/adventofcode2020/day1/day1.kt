216dc8e3169df71bd79ca0d2388a618b4b732971
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-01 13:19:06 -0300

    Day 1 part 2

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day1/day1.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day1/day1.kt
@@ -1,11 +1,14 @@
 package com.lucaszeta.adventofcode2020.day1
 
+private fun List<Int>.listWithRemainingNumbers(currentIndex: Int) =
+    filterIndexed { index, _ -> index > currentIndex }
+
 fun findPair(
     input: List<Int>,
     sumGoal: Int
 ): Pair<Int, Int> {
     for ((index, operand1) in input.dropLast(1).withIndex()) {
-        val remainingNumbers = input.filterIndexed { secondIndex, _ -> secondIndex > index }
+        val remainingNumbers = input.listWithRemainingNumbers(index)
 
         for (operand2 in remainingNumbers) {
             if (operand1 + operand2 == sumGoal) {
@@ -22,10 +25,10 @@ fun findTriple(
     sumGoal: Int
 ): Triple<Int, Int, Int> {
     for ((index, operand1) in input.dropLast(1).withIndex()) {
-        val secondGroup = input.filterIndexed { secondIndex, _ -> secondIndex > index }
+        val secondGroup = input.listWithRemainingNumbers(index)
 
-        for ((index2, operand2) in secondGroup.dropLast(1).withIndex()) {
-            val remainingNumbers = input.filterIndexed { thirdIndex, _ -> thirdIndex > index2 }
+        for ((secondIndex, operand2) in secondGroup.dropLast(1).withIndex()) {
+            val remainingNumbers = input.listWithRemainingNumbers(secondIndex)
 
             for (operand3 in remainingNumbers) {
                 if (operand1 + operand2 + operand3 == sumGoal) {
@@ -39,7 +42,9 @@ fun findTriple(
 }
 
 fun main() {
-    val operands = findPair(input, 2020)
+    val pair = findPair(input, 2020)
+    val triple = findTriple(input, 2020)
 
-    println(operands.first * operands.second)
+    println("Day 1 part 1: %d".format(pair.first * pair.second))
+    println("Day 1 part 2: %d".format(triple.first * triple.second * triple.third))
 }
