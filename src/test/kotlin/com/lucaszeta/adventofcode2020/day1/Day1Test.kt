e3caaf2dbb91b3cdf8cd2490e416a13fa8e75ddc
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-01 18:16:55 -0300

    Cover unhappy paths with tests

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day1/Day1Test.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day1/Day1Test.kt
@@ -1,21 +1,41 @@
 package com.lucaszeta.adventofcode2020.day1
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 
 class Day1Test {
 
     @Test
-    fun shouldFindPairThatSumTo2020() {
+    fun `Should find pair that sum to 2020`() {
         val input = listOf(1721, 979, 366, 299, 675, 1456)
 
         assertEquals(1721 to 299, findPair(input, 2020))
     }
 
     @Test
-    fun shouldFindTripleThatSumTo2020() {
+    fun `Should throw exception when not finding pair that sum to 2020`() {
+        val input = listOf(1721, 979, 675, 1456)
+
+        assertThrows(IllegalArgumentException::class.java) {
+            findPair(input, 2020)
+        }
+    }
+
+    @Test
+    fun `Should find triple that sum to 2020`() {
         val input = listOf(1721, 979, 366, 299, 675, 1456)
 
         assertEquals(Triple(979, 366, 675), findTriple(input, 2020))
+    }
+
+    @Test
+    fun `Should throw exception when not finding triple that sum to 2020`() {
+        val input = listOf(1721, 979, 299, 675, 1456)
+
+        assertThrows(IllegalArgumentException::class.java) {
+            findTriple(input, 2020)
+        }
     }
 }
