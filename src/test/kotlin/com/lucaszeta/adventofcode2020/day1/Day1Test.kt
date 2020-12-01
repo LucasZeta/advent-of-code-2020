315aed0ceefd983eb94254eaf5de1893e572ef26
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-01 19:47:58 -0300

    Assert list sorting does not affect the result

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day1/Day1Test.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day1/Day1Test.kt
@@ -15,6 +15,13 @@ class Day1Test {
     }
 
     @Test
+    fun `Should find pair that sum to 2020, regardless of the list sorting`() {
+        val input = listOf(1456, 979, 366, 675, 299, 1721)
+
+        assertEquals(299 to 1721, findPair(input, 2020))
+    }
+
+    @Test
     fun `Should throw exception when not finding pair that sum to 2020`() {
         val input = listOf(1721, 979, 675, 1456)
 
@@ -28,6 +35,13 @@ class Day1Test {
         val input = listOf(1721, 979, 366, 299, 675, 1456)
 
         assertEquals(Triple(979, 366, 675), findTriple(input, 2020))
+    }
+
+    @Test
+    fun `Should find triple that sum to 2020, regardless of the list sorting`() {
+        val input = listOf(366, 979, 675, 1456, 1721, 299)
+
+        assertEquals(Triple(366, 979, 675), findTriple(input, 2020))
     }
 
     @Test
