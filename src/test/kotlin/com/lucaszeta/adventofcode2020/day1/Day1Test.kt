effd33087caa1f6b54747a96ee743ad51ba1b352
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-01 18:06:21 -0300

    Add JUnit 5 support

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day1/Day1Test.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day1/Day1Test.kt
@@ -1,7 +1,7 @@
 package com.lucaszeta.adventofcode2020.day1
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class Day1Test {
 
