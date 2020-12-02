dcf7e26fdd709e729962ff5990fabb57375e6b61
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-02 12:07:02 -0300

    Map string to object using reference

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day2/day2.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day2/day2.kt
@@ -1,10 +1,10 @@
 package com.lucaszeta.adventofcode2020.day2
 
 fun countSledRentalValidPasswords(input: List<String>) =
-    input.map { PasswordValidation(it) }.count { it.isSledRentalValid() }
+    input.map(::PasswordValidation).count { it.isSledRentalValid() }
 
 fun countTobogganValidPasswords(input: List<String>) =
-    input.map { PasswordValidation(it) }.count { it.isTobogganValid() }
+    input.map(::PasswordValidation).count { it.isTobogganValid() }
 
 fun main() {
     println("Sled Rental valid passwords: %d".format(countSledRentalValidPasswords(input)))
