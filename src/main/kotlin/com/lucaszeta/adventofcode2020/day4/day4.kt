469b38dcac63d44efea452e101c16a850a9a8078
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-04 08:40:30 -0300

    Day 4 part 1

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day4/day4.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day4/day4.kt
@@ -1,5 +1,7 @@
 package com.lucaszeta.adventofcode2020.day4
 
+import com.lucaszeta.adventofcode2020.ext.getResourceAsText
+
 fun parseData(input: String) = input
     .replace("([a-z0-9])\\n([a-z0-9])".toRegex()) {
         "%s %s".format(
@@ -9,3 +11,12 @@ fun parseData(input: String) = input
     }
     .replace("\n\n", "\n")
     .split("\n")
+
+fun countValidPassports(input: List<String>) =
+    input.map(::Passport).count { it.isValid() }
+
+fun main() {
+    val input = parseData(getResourceAsText("/day4/passport-batch-file.txt"))
+
+    println("Valid passports: %d".format(countValidPassports(input)))
+}
