2f210c69ed5bd566976b0e6a4392b429e7429136
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-06 09:42:45 -0300

    Day 6 part 1

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day6/day6.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day6/day6.kt
@@ -1,5 +1,17 @@
 package com.lucaszeta.adventofcode2020.day6
 
+import com.lucaszeta.adventofcode2020.ext.getResourceAsText
+
+fun main() {
+    val input = parseData(getResourceAsText("/day6/customs-form-answers.txt"))
+
+    val sumQuestionsAnswered = input
+        .map(::countQuestions)
+        .reduce { acc, questionCount -> acc + questionCount }
+
+    println("Sum of questions answered: $sumQuestionsAnswered")
+}
+
 fun countQuestions(questions: String) =
     questions.toSet().size
 
