241f5c5845dcd33b45881369b73abb5ef76d8bb3
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-09 09:16:55 -0300

    Assert exceptions are thrown when not finding the numbers

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day9/Day9Test.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day9/Day9Test.kt
@@ -2,35 +2,27 @@ package com.lucaszeta.adventofcode2020.day9
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 class Day9Test {
 
     @Test
     fun `Should find number not product of sum of previous numbers`() {
-        val input = listOf(
-            35,
-            20,
-            15,
-            25,
-            47,
-            40,
-            62,
-            55,
-            65,
-            95,
-            102,
-            117,
-            150,
-            182,
-            127,
-            219,
-            299,
-            277,
-            309,
-            576
-        ).map { it.toLong() }
+        val input = listOf(35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576)
+            .map { it.toLong() }
 
         assertEquals(127, findNumberNotSumOfPrevious(input, 5))
+    }
+
+    @Test
+    fun `Should throw exception when all numbers are product of sum of previous numbers`() {
+        val input = listOf(0, 0, 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+            .map { it.toLong() }
+
+        assertThrows<IllegalArgumentException> {
+            findNumberNotSumOfPrevious(input, 5)
+        }
     }
 
     @Test
@@ -44,6 +36,16 @@ class Day9Test {
                 listOf(15L, 25L, 47L, 40L),
                 findNumbersThatSumToTarget(numbers.map { it.toLong() }, 127)
             )
+        }
+    }
+
+    @Test
+    fun `Should throw exception when not finding adjacent numbers that sum to target`() {
+        val input = listOf(35, 20, 15, 25, 47, 43, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576)
+            .map { it.toLong() }
+
+        assertThrows<IllegalArgumentException> {
+            findNumbersThatSumToTarget(input, 127)
         }
     }
 
