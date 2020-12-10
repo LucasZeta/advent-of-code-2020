f445588da4e82f65e6df60955dd5f74398a582fd
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-10 07:56:33 -0300

    Group differences between joltages

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day10/day10.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day10/day10.kt
@@ -1,5 +1,21 @@
 package com.lucaszeta.adventofcode2020.day10
 
+fun List<Int>.findJoltageGroupDifferences(): Map<Int, Int> {
+    val sortedValues = sorted()
+
+    return sortedValues
+        .mapIndexed { index, joltage ->
+            if (index > 0) {
+                joltage - sortedValues[index - 1]
+            } else 0
+        }
+        .filter { it != 0 }
+        .groupBy { it }
+        .mapValues {
+            it.value.size
+        }
+}
+
 fun findDeviceAdapterJoltage(bagAdapterJoltages: List<Int>) =
     (bagAdapterJoltages.maxOrNull() ?: 0) + 3
 
--- src/test/kotlin/com/lucaszeta/adventofcode2020/day10/Day10KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day10/Day10KtTest.kt
@@ -11,4 +11,15 @@ class Day10KtTest {
 
         assertEquals(22, findDeviceAdapterJoltage(input))
     }
+
+    @Test
+    fun `Should map joltage differences`() {
+        val input = listOf(0, 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4, 22)
+        val expectedOutput = mapOf(
+            1 to 7,
+            3 to 5
+        )
+
+        assertEquals(expectedOutput, input.findJoltageGroupDifferences())
+    }
 }
