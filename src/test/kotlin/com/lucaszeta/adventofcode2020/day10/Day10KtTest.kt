72eaf0831ecb29937fa1bf746cf39583f5630b62
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-10 14:18:46 -0300

    Split smaller and bigger inputs into two tests

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day10/Day10KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day10/Day10KtTest.kt
@@ -21,31 +21,37 @@ class Day10KtTest {
         )
 
         assertEquals(expectedOutput, input.findJoltageGroupDifferences())
+    }
 
-        val input2 = listOf(
+    @Test
+    fun `Should map joltage differences for bigger inputs`() {
+        val input = listOf(
             0, 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19,
             38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3, 52
         )
-
-        val expectedOutput2 = mapOf(
+        val expectedOutput = mapOf(
             1 to 22,
             3 to 10
         )
-        assertEquals(expectedOutput2, input2.findJoltageGroupDifferences())
+
+        assertEquals(expectedOutput, input.findJoltageGroupDifferences())
     }
 
     @Test
     fun `Should count possible arrangements`() {
         val input = listOf(0, 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4, 22)
 
-        assertEquals(8, input.findArrangements())
+        assertEquals(8, input.calculateTotalArrangements())
+    }
 
-        val input2 = listOf(
+    @Test
+    fun `Should count possible arrangements for bigger inputs`() {
+        val input = listOf(
             0, 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19,
             38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3, 52
         )
 
-        assertEquals(19208, input2.findArrangements())
+        assertEquals(19208, input.calculateTotalArrangements())
     }
 
     @Test
