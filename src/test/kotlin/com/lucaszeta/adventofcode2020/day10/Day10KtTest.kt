7b27637bd458512fc515b71ccda76d51b340b2d2
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-10 13:57:35 -0300

    Assert group differences for bigger inputs

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day10/Day10KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day10/Day10KtTest.kt
@@ -21,5 +21,16 @@ class Day10KtTest {
         )
 
         assertEquals(expectedOutput, input.findJoltageGroupDifferences())
+
+        val input2 = listOf(
+            0, 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19,
+            38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3, 52
+        )
+
+        val expectedOutput2 = mapOf(
+            1 to 22,
+            3 to 10
+        )
+        assertEquals(expectedOutput2, input2.findJoltageGroupDifferences())
     }
 }
