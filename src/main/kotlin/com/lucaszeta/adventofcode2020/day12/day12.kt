927cae4bae8c1eae8ffd9bccff6ef2f52cfb6a09
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-12 09:49:40 -0300

    Solve day 12 part 1

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day12/day12.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day12/day12.kt
@@ -1,6 +1,26 @@
 package com.lucaszeta.adventofcode2020.day12
 
+import com.lucaszeta.adventofcode2020.ext.getResourceAsText
 import kotlin.math.abs
+
+fun main() {
+    val instructions = getResourceAsText("/day12/navigation-instructions.txt")
+        .split("\n")
+        .filter { it.isNotEmpty() }
+        .map(::NavigationalInstruction)
+
+    val navigationalSystem = FerryBoatNavigationalSystem(instructions)
+    navigationalSystem.navigate()
+
+    val distance = calculateManhattanDistance(
+        navigationalSystem.currentPositionX,
+        navigationalSystem.currentPositionY,
+        0,
+        0
+    )
+
+    println("Distance from origin: $distance")
+}
 
 fun calculateManhattanDistance(
     currentX: Int,
