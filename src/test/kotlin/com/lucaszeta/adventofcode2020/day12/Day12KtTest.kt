3bbd83964f50c9a2bbb7d1128e6b65e2a474d3dd
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-12 18:39:11 -0300

    Fix test not being run

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day12/Day12KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day12/Day12KtTest.kt
@@ -1,9 +1,11 @@
 package com.lucaszeta.adventofcode2020.day12
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class Day12KtTest {
 
+    @Test
     fun `Should calculate distance between origin and current position`() {
         assertEquals(25, calculateManhattanDistance(17, -8, 0, 0))
     }
