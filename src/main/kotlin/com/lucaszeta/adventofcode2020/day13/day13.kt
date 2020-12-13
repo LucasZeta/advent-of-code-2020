9b1942185b33944fa57c9f477c0a590e847635f6
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-13 09:06:37 -0300

    Parse input data

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day13/day13.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day13/day13.kt
@@ -7,3 +7,16 @@ fun calculateNearestBusArrival(busIds: List<Int>, earliestTimestamp: Int) = busI
         busId to busId - earliestTimestamp.rem(busId)
     }
     .minByOrNull { it.second } ?: throw IllegalArgumentException("Invalid input")
+
+fun parseData(input: String) = input
+    .split("\n")
+    .filter { it.isNotEmpty() }
+    .run {
+        val timestamp = first().toInt()
+        val busIds = "(\\d+)".toRegex()
+            .findAll(last())
+            .map { it.groupValues[1].toInt() }
+            .toList()
+
+        Pair(timestamp, busIds)
+    }
--- src/test/kotlin/com/lucaszeta/adventofcode2020/day13/Day13KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day13/Day13KtTest.kt
@@ -6,6 +6,16 @@ import org.junit.jupiter.api.Test
 class Day13KtTest {
 
     @Test
+    fun `Should parse input data`() {
+        val input = "939\n" +
+            "7,13,x,x,59,x,31,19\n" +
+            ""
+        val expectedOutput = 939 to listOf(7, 13, 59, 31, 19)
+
+        assertEquals(expectedOutput, parseData(input))
+    }
+
+    @Test
     fun `Should find nearest bus arrival`() {
         val timestamp = 939
         val busIds = listOf(7, 13, 59, 31, 19)
