0515f039c08b7441e86274bc5e462865cd98004a
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-16 07:01:52 -0300

    Find values that are not in any ticket field's range

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day16/day16.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day16/day16.kt
@@ -1,5 +1,24 @@
 package com.lucaszeta.adventofcode2020.day16
 
+fun findInvalidFields(
+    nearbyTickets: List<List<Int>>,
+    ticketFields: Map<String, List<IntRange>>
+): List<Int> {
+    val invalidValues = mutableListOf<Int>()
+
+    for (value in nearbyTickets.flatten()) {
+        val valid = ticketFields.any {
+            value in it.value.first() || value in it.value.last()
+        }
+
+        if (!valid) {
+            invalidValues.add(value)
+        }
+    }
+
+    return invalidValues.toList()
+}
+
 fun extractYourTicket(input: String): List<Int> {
     val lines = input.split("\n")
     val index = lines.indexOf("your ticket:")
--- src/test/kotlin/com/lucaszeta/adventofcode2020/day16/Day16KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day16/Day16KtTest.kt
@@ -73,4 +73,24 @@ class Day16KtTest {
 
         assertEquals(expectedOutput, extractNearbyTickets(input))
     }
+
+    @Test
+    fun `Should find values that are not in any range`() {
+        val fields = mapOf(
+            "class" to listOf(1..3, 5..7),
+            "row" to listOf(6..11, 33..44),
+            "seat" to listOf(13..40, 45..50)
+        )
+
+        val nearbyTickets = listOf(
+            listOf(7, 3, 47),
+            listOf(40, 4, 50),
+            listOf(55, 2, 20),
+            listOf(38, 6, 12),
+        )
+
+        val expectedOutput = listOf(4, 55, 12)
+
+        assertEquals(expectedOutput, findInvalidFields(nearbyTickets, fields))
+    }
 }
