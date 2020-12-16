6047c41dad78b30d03d9ea544028607620c92999
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-15 23:25:17 -0300

    Add test for bigger inputs
    For the sample input there is a OutOfMemoryError happening

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day15/Day15KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day15/Day15KtTest.kt
@@ -6,20 +6,6 @@ import org.junit.jupiter.api.Test
 class Day15KtTest {
 
     @Test
-    fun `Should return 0 if number is new in the list`() {
-        val input = listOf(0, 3, 6)
-
-        assertEquals(0, nextNumber(input))
-    }
-
-    @Test
-    fun `Should return sum of last two indexes if number is not new in the list`() {
-        val input = listOf(0, 3, 6, 0)
-
-        assertEquals(3, nextNumber(input))
-    }
-
-    @Test
     fun `Should iterate until nth number is found`() {
         listOf(
             listOf(1, 3, 2) to 1,
@@ -32,6 +18,23 @@ class Day15KtTest {
             assertEquals(
                 expectedOutput,
                 findNthNumber(input.toMutableList(), 2020)
+            )
+        }
+    }
+
+    @Test
+    fun `Should iterate until a big nth number is found`() {
+        listOf(
+            listOf(1, 3, 2) to 0,
+            listOf(2, 1, 3) to 2419,
+            listOf(1, 2, 3) to 186,
+            listOf(2, 3, 1) to 10,
+            listOf(3, 2, 1) to 1437890,
+            listOf(3, 1, 2) to 0
+        ).forEach { (input, expectedOutput) ->
+            assertEquals(
+                expectedOutput,
+                findNthNumber(input, 3000000)
             )
         }
     }
