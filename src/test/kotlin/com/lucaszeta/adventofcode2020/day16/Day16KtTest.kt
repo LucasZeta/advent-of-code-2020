aaa401568d1fb27ba5cf4ff950b0d45d2c7ccf74
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-16 23:41:07 -0300

    Fix lint

--- src/test/kotlin/com/lucaszeta/adventofcode2020/day16/Day16KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day16/Day16KtTest.kt
@@ -8,17 +8,17 @@ class Day16KtTest {
     @Test
     fun `Should read ticket fields`() {
         val input = "class: 1-3 or 5-7\n" +
-                "row: 6-11 or 33-44\n" +
-                "seat: 13-40 or 45-50\n" +
-                "\n" +
-                "your ticket:\n" +
-                "7,1,14\n" +
-                "\n" +
-                "nearby tickets:\n" +
-                "7,3,47\n" +
-                "40,4,50\n" +
-                "55,2,20\n" +
-                "38,6,12"
+            "row: 6-11 or 33-44\n" +
+            "seat: 13-40 or 45-50\n" +
+            "\n" +
+            "your ticket:\n" +
+            "7,1,14\n" +
+            "\n" +
+            "nearby tickets:\n" +
+            "7,3,47\n" +
+            "40,4,50\n" +
+            "55,2,20\n" +
+            "38,6,12"
 
         val expectedOutput = mapOf(
             "class" to listOf(1..3, 5..7),
@@ -32,17 +32,17 @@ class Day16KtTest {
     @Test
     fun `Should read your ticket`() {
         val input = "class: 1-3 or 5-7\n" +
-                "row: 6-11 or 33-44\n" +
-                "seat: 13-40 or 45-50\n" +
-                "\n" +
-                "your ticket:\n" +
-                "7,1,14\n" +
-                "\n" +
-                "nearby tickets:\n" +
-                "7,3,47\n" +
-                "40,4,50\n" +
-                "55,2,20\n" +
-                "38,6,12"
+            "row: 6-11 or 33-44\n" +
+            "seat: 13-40 or 45-50\n" +
+            "\n" +
+            "your ticket:\n" +
+            "7,1,14\n" +
+            "\n" +
+            "nearby tickets:\n" +
+            "7,3,47\n" +
+            "40,4,50\n" +
+            "55,2,20\n" +
+            "38,6,12"
 
         val expectedOutput = listOf(7, 1, 14)
 
@@ -52,17 +52,17 @@ class Day16KtTest {
     @Test
     fun `Should read nearby tickets`() {
         val input = "class: 1-3 or 5-7\n" +
-                "row: 6-11 or 33-44\n" +
-                "seat: 13-40 or 45-50\n" +
-                "\n" +
-                "your ticket:\n" +
-                "7,1,14\n" +
-                "\n" +
-                "nearby tickets:\n" +
-                "7,3,47\n" +
-                "40,4,50\n" +
-                "55,2,20\n" +
-                "38,6,12"
+            "row: 6-11 or 33-44\n" +
+            "seat: 13-40 or 45-50\n" +
+            "\n" +
+            "your ticket:\n" +
+            "7,1,14\n" +
+            "\n" +
+            "nearby tickets:\n" +
+            "7,3,47\n" +
+            "40,4,50\n" +
+            "55,2,20\n" +
+            "38,6,12"
 
         val expectedOutput = listOf(
             listOf(7, 3, 47),
