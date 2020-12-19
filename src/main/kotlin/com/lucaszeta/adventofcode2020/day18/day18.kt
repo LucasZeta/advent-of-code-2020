52e4f5887e729099ad35859ac0dbd2e2180b70ea
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-19 00:07:58 -0300

    Remove operand nullability

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day18/day18.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day18/day18.kt
@@ -1,8 +1,8 @@
 package com.lucaszeta.adventofcode2020.day18
 
 fun evaluateExpression(expression: String): Int {
-    var operand1: Int? = null
-    var operand2: Int? = null
+    var operand1 = 0
+    var operand2 = 0
     var operator: ((Int, Int) -> Int)? = null
 
     val numbersPattern = "(\\d+)".toRegex()
@@ -14,7 +14,7 @@ fun evaluateExpression(expression: String): Int {
             "+" -> operator = Int::plus
             "*" -> operator = Int::times
             "(" -> newDigit = evaluateExpression(expression.drop(index + 1))
-            ")" -> return operand1 ?: 0
+            ")" -> return operand1
             else -> {
                 if (numbersPattern.matches(instruction)) {
                     newDigit = instruction.toInt()
@@ -23,24 +23,22 @@ fun evaluateExpression(expression: String): Int {
         }
 
         if (newDigit > 0) {
-            if (operand1 == null) {
+            if (operand1 == 0) {
                 operand1 = instruction.toInt()
             } else {
                 operand2 = instruction.toInt()
             }
         }
 
-        if (operand1 != null && operator != null && operand2 != null) {
-            val a = operand1!!
-            val b = operand2!!
-            val operation = operator!!
-
-            operand1 = operation(a, b)
+        if (operand1 > 0 && operator != null && operand2 > 0) {
+            operator?.let {
+                operand1 = it.invoke(operand1, operand2)
+            }
 
             operator = null
-            operand2 = null
+            operand2 = 0
         }
     }
 
-    return operand1 ?: 0
+    return operand1
 }
