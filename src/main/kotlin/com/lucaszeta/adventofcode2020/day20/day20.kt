f1d7d1ae412682839e5ebcc1bffbec18b7710ec4
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-21 09:11:09 -0300

    Solve day 21 part 1

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day20/day20.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day20/day20.kt
@@ -1,5 +1,19 @@
 package com.lucaszeta.adventofcode2020.day20
 
+import com.lucaszeta.adventofcode2020.ext.getResourceAsText
+
+fun main() {
+    val foodList = getResourceAsText("/day20/food-list.txt")
+        .split("\n")
+        .filter { it.isNotEmpty() }
+        .map(::Food)
+
+    val riskyIngredients = findRiskyIngredients(foodList)
+    val safeIngredients = findSafeIngredients(foodList, riskyIngredients)
+
+    println("Safe ingredients count: ${safeIngredients.size}")
+}
+
 fun findRiskyIngredients(foodList: List<Food>): Set<String> {
     val riskyIngredients = mutableMapOf<String, MutableSet<String>>()
 
