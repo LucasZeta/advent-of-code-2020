d74bf3163260f194e7ffbc4c7cf30c8c2171353f
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-22 23:04:29 -0300

    Solve day 20 part 1

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day20/day20.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day20/day20.kt
@@ -1,5 +1,20 @@
 package com.lucaszeta.adventofcode2020.day20
 
+import com.lucaszeta.adventofcode2020.ext.getResourceAsText
+
+fun main() {
+    val tiles = getResourceAsText("/day20/tiles.txt")
+        .split("\n\n")
+        .map(::ImageTile)
+
+    val cornerTiles = findCornerTiles(tiles)
+    val cornerTilesProduct = cornerTiles
+        .map { it.first }
+        .reduce(Long::times)
+
+    println("Product of corner tiles ID $cornerTilesProduct")
+}
+
 fun findCornerTiles(allTiles: List<ImageTile>) = allTiles.run {
     val cornerCount = 2
     val cornersList = map { it.id to it.findCorners() }
