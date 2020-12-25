0a83318c73cfc8923ca8f595b7d0776a54d264b9
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-25 12:14:38 -0300

    Generate public key

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day25/day25.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day25/day25.kt
@@ -1,5 +1,18 @@
 package com.lucaszeta.adventofcode2020.day25
 
+fun generateEncryptionKey(
+    subjectNumber: Int,
+    loopSize: Int
+): Long {
+    var accumulator = 1L
+
+    repeat(loopSize) {
+        accumulator = (accumulator * subjectNumber).rem(20201227)
+    }
+
+    return accumulator
+}
+
 fun findLoopSize(publicKey: Int, subjectNumber: Int): Int {
     var loopSize = 0
     var accumulator = 1
--- src/test/kotlin/com/lucaszeta/adventofcode2020/day25/Day25KtTest.kt
+++ src/test/kotlin/com/lucaszeta/adventofcode2020/day25/Day25KtTest.kt
@@ -6,6 +6,17 @@ import org.junit.jupiter.api.Test
 class Day25KtTest {
 
     @Test
+    fun `Should generate encryption key`() {
+        val expectedEncryptionKey = 14897079L
+
+        val encryptionKey1 = generateEncryptionKey(5764801, 11)
+        val encryptionKey2 = generateEncryptionKey(17807724, 8)
+
+        assertEquals(expectedEncryptionKey, encryptionKey1)
+        assertEquals(expectedEncryptionKey, encryptionKey2)
+    }
+
+    @Test
     fun `Should discover loop size`() {
         assertEquals(8, findLoopSize(5764801, 7))
         assertEquals(11, findLoopSize(17807724, 7))
