fda362c1e8c8332fbff0eca12d4494c09602099c
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-12-25 12:16:35 -0300

    Solve day 25

--- src/main/kotlin/com/lucaszeta/adventofcode2020/day25/day25.kt
+++ src/main/kotlin/com/lucaszeta/adventofcode2020/day25/day25.kt
@@ -1,5 +1,25 @@
 package com.lucaszeta.adventofcode2020.day25
 
+import com.lucaszeta.adventofcode2020.ext.getResourceAsText
+
+const val SUBJECT_NUMBER = 7
+
+fun main() {
+    val (cardPublicKey, doorPublicKey) = getResourceAsText("/day25/public-keys.txt")
+        .split("\n")
+        .filter { it.isNotEmpty() }
+        .map { it.toInt() }
+
+    val cardLoopSize = findLoopSize(cardPublicKey, SUBJECT_NUMBER)
+    val doorLoopSize = findLoopSize(doorPublicKey, SUBJECT_NUMBER)
+
+    val encryptionKey1 = generateEncryptionKey(cardPublicKey, doorLoopSize)
+    val encryptionKey2 = generateEncryptionKey(doorPublicKey, cardLoopSize)
+
+    println("Loop sizes: $cardLoopSize and $doorLoopSize")
+    println("Encryption keys: $encryptionKey1 and $encryptionKey2")
+}
+
 fun generateEncryptionKey(
     subjectNumber: Int,
     loopSize: Int
